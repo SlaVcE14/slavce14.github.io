2ac0fc638936aa64b8a60f0564c33d393d5f29b0
SlaVcE <slavce14.apps@gmail.com>
2025-02-12 19:02:03 +0100

    update

--- scriptHome.js
+++ scriptHome.js
@@ -6,11 +6,3 @@ if (noSplash){
     linkNode.parentNode.removeChild(linkNode);
     document.getElementById("logo1").style.visibility = "hidden";
 }
-
-window.addEventListener("DOMContentLoaded", function () {
-    console.log(document.referrer)
-    console.log(location.hostname)
-    if (document.referrer && !document.referrer.includes(location.hostname)) {
-        console.log("User came back from another site!");
-    }
-});
