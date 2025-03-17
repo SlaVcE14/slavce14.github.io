0e8ef46df2d0e8d0a1aa00dfcadcd254434a5765
SlaVcE <slavce14.apps@gmail.com>
2025-03-17 01:06:48 +0100

    update script

--- scriptHome.js
+++ scriptHome.js
@@ -2,7 +2,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 let noSplash = urlParams.has('noSplash');
 if (noSplash){
-    let linkNode = document.getElementsByTagName('link')[3];
+    let linkNode = document.getElementsByTagName('link')[2];
     linkNode.parentNode.removeChild(linkNode);
     document.getElementById("logo1").style.visibility = "hidden";
 }
